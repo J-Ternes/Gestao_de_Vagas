a2d88f0ecbbaa642d7d0092edcaa1d5b787f4fa1
Jonathan Ternes <ternes.jonathangabriel@gmail.com>
2025-11-20 19:37:56 -0300

    Adicionando docker e postgres

--- src/main/java/com/jonathan/Gestao_Vagas/modules/candidates/CandidateEntity.java
+++ src/main/java/com/jonathan/Gestao_Vagas/modules/candidates/CandidateEntity.java
@@ -1,16 +1,26 @@
 package com.jonathan.Gestao_Vagas.modules.candidates;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 
 @Data //Adiciona todos os getters e setters dos atributos
+@Entity(name = "candidate") //criação da tabela
 public class CandidateEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID) //Cria o id automaticamente quando inserido um dado
     private UUID id;
+
     private String name;
 
     @Pattern(regexp = "\\S+", message = "O campo do usarname não pode conter espaços.")
@@ -24,5 +34,8 @@ public class CandidateEntity {
 
     private String curriculum;
     private String description;
+
+    @CreationTimestamp //pega a data e hora exata da criação do dado no banco de dados
+    private LocalDateTime creatdAt;
 
 }
