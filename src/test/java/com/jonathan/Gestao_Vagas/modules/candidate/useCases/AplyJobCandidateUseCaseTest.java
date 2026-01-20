ed0fe080f3d9a6f1e797452de956852e9065d3e4
Jonathan Ternes <ternes.jonathangabriel@gmail.com>
2026-01-20 16:39:24 -0300

    Testando a resposta da exceção JobNotFoundException

--- src/test/java/com/jonathan/Gestao_Vagas/modules/candidate/useCases/AplyJobCandidateUseCaseTest.java
+++ src/test/java/com/jonathan/Gestao_Vagas/modules/candidate/useCases/AplyJobCandidateUseCaseTest.java
@@ -1,7 +1,9 @@
 package com.jonathan.Gestao_Vagas.modules.candidate.useCases;
 
+import com.jonathan.Gestao_Vagas.exceptions.JobNotFoundException;
 import com.jonathan.Gestao_Vagas.exceptions.UserNotFoundException;
 import com.jonathan.Gestao_Vagas.modules.candidates.controllers.CandidateRepository;
+import com.jonathan.Gestao_Vagas.modules.candidates.entities.CandidateEntity;
 import com.jonathan.Gestao_Vagas.modules.candidates.useCases.AplyJobCandidateUseCase;
 import com.jonathan.Gestao_Vagas.modules.company.repositories.JobRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -11,7 +13,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -33,6 +39,22 @@ public class AplyJobCandidateUseCaseTest {
             aplyJobCandidateUseCase.execute(null,null);
         } catch (Exception e) {
             assertThat(e).isInstanceOf(UserNotFoundException.class); //Para assegurar que a instancia passada seja essa
+        }
+    }
+
+    @Test
+    public void should_not_be_able_to_aply_job_with_job_not_found(){
+
+        var idCandidate = UUID.randomUUID();
+        var candidade = new CandidateEntity();
+        candidade.setId(idCandidate);
+
+        when(candidateRepository.findById(idCandidate)).thenReturn(Optional.of(candidade));
+
+        try{
+            aplyJobCandidateUseCase.execute(idCandidate,null);
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(JobNotFoundException.class); //Para assegurar que a instancia passada seja essa
         }
     }
 
